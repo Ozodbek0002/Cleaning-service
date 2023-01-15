99abaae984e4c0291b8810c90f0a1b55c5cd4d62
Ozodbek <ozodbekozodov0002@gmail.com>
2023-01-15 17:37:46 +0500

    auth

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -7,6 +7,10 @@ use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
+use App\Events\PostCreated;
+use App\Listeners\SendEmailToUser;
+use App\Listeners\SendNotificationToAdmin;
+
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -17,6 +21,11 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+
+        PostCreated::class => [
+            SendEmailToUser::class,
+            SendNotificationToAdmin::class,
         ],
     ];
 
