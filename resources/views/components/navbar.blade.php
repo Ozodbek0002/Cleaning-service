5f2872158fb5bc9178a4ca104c71a6c849d692b0
Ozodbek <ozodbekozodov0002@gmail.com>
2023-01-20 22:10:26 +0500

    notivication

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,25 +8,35 @@
     <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
         <div class="navbar-nav mr-auto py-0">
             <a href="{{ url('/') }}" class="nav-item nav-link {{ request()->routeIs('/') ? 'active' : '' }}">Asosiy</a>
-            <a href="{{ route('about') }}" class="nav-item nav-link {{ request()->routeIs('about') ? 'active' : '' }}">Biz haqimizda</a>
-            <a href="{{ route('services') }}" class="nav-item nav-link {{ request()->routeIs('services') ? 'active' : '' }}">Servislar</a>
-            <a href="{{route('projects')}}" class="nav-item nav-link {{ request()->routeIs('projects') ? 'active' : '' }}">Loyihalar</a>
-            <a href="{{route('posts.index')}}" class="nav-item nav-link {{ request()->routeIs('posts.index') ? 'active' : '' }}">Bloglar</a>
-            <a href="{{route('contact')}}" class="nav-item nav-link {{ request()->routeIs('contact') ? 'active' : '' }}">Aloqa</a>
+            <a href="{{ route('about') }}" class="nav-item nav-link {{ request()->routeIs('about') ? 'active' : '' }}">Biz
+                haqimizda</a>
+            <a href="{{ route('services') }}"
+               class="nav-item nav-link {{ request()->routeIs('services') ? 'active' : '' }}">Servislar</a>
+            <a href="{{route('projects')}}"
+               class="nav-item nav-link {{ request()->routeIs('projects') ? 'active' : '' }}">Loyihalar</a>
+            <a href="{{route('posts.index')}}"
+               class="nav-item nav-link {{ request()->routeIs('posts.index') ? 'active' : '' }}">Bloglar</a>
+            <a href="{{route('contact')}}"
+               class="nav-item nav-link {{ request()->routeIs('contact') ? 'active' : '' }}">Aloqa</a>
         </div>
         @auth()
 
             <div>
-                <a href="" class=" mr-4 rounded" >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
+                <a href="" class=" btn btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"/>
                     </svg>
+
+                    <span class="badge badge-light"> 9 </span>
+                    <span class="sr-only">  </span>
                 </a>
             </div>
 
-        <div style="width: 10px;">
+            <div style="width: 10px;">
 
-        </div>
+            </div>
 
             <div>
                 {{ Auth::user()->name }}
