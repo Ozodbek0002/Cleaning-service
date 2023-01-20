835cda68c93a97fed00a6025ea7fff257107b32f
Ozodbek <ozodbekozodov0002@gmail.com>
2023-01-21 00:02:04 +0500

    notivication

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Notification;
 class NotificationController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('notifications.index',[
