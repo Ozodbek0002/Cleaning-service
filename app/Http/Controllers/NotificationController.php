0ea7564acdfa0a001128452f1c48063f6ce61152
Ozodbek <ozodbekozodov0002@gmail.com>
2023-01-21 21:12:51 +0500

    notivication finish

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -26,31 +26,6 @@ class NotificationController extends Controller implements ShouldQueue
 
 
 
-    public function create()
-    {
-        //
-    }
-    public function store(Request $request)
-    {
-        //
-    }
-    public function show($id)
-    {
-        //
-    }
-    public function edit($id)
-    {
-        //
-    }
-    public function update(Request $request, $id)
-    {
-        //
-    }
-    public function destroy($id)
-    {
-        //
-    }
-
     public function markAsRead(DatabaseNotification $notification)
     {
         $notification->markAsRead();
