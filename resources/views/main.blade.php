93fbf58f4d503093b3b17d22e2d71afa5d2bf9ee
Ozodbek <ozodbekozodov0002@gmail.com>
2023-01-28 23:50:50 +0500

    lang

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -28,9 +28,9 @@
                     <img class="img-fluid" src="img/carousel-2.jpg" alt="Image">
                     <div class="carousel-caption d-flex align-items-center justify-content-center">
                         <div class="p-5" style="width: 100%; max-width: 900px;">
-                            <h5 class="text-primary text-uppercase mb-md-3">Cleaning Services</h5>
+                            <h5 class="text-primary text-uppercase mb-md-3">{{__('Tozalash xizmatlari')}}</h5>
                             <h1 class="display-3 text-white mb-md-4">Highly Professional Cleaning Services</h1>
-                            <a href="" class="btn btn-primary">Get A Quote</a>
+{{--                            <a href="" class="btn btn-primary">Get A Quote</a>--}}
                         </div>
                     </div>
                 </div>
@@ -38,9 +38,9 @@
                     <img class="img-fluid" src="img/carousel-3.jpg" alt="Image">
                     <div class="carousel-caption d-flex align-items-center justify-content-center">
                         <div class="p-5" style="width: 100%; max-width: 900px;">
-                            <h5 class="text-primary text-uppercase mb-md-3">Cleaning Services</h5>
+                            <h5 class="text-primary text-uppercase mb-md-3">{{__('Tozalash xizmatlari')}}</h5>
                             <h1 class="display-3 text-white mb-md-4">Experienced & Expert Cleaners</h1>
-                            <a href="" class="btn btn-primary">Get A Quote</a>
+{{--                            <a href="" class="btn btn-primary">Get A Quote</a>--}}
                         </div>
                     </div>
                 </div>
