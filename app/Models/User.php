ea02b19854732ec2bf7d6f6fd4a0795b37d6ec5c
Ozodbek <ozodbekozodov0002@gmail.com>
2023-02-09 22:48:43 +0500

    Stubs

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,11 +32,7 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
