1ea46b24c4fc74f5efdc9f62e247d58bf09a89be
Ozodbek <ozodbekozodov0002@gmail.com>
2023-02-09 22:45:02 +0500

    Stubs

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,11 +26,7 @@ class User extends Authenticatable
         'photo',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+
     protected $hidden = [
         'password',
         'remember_token',
